28e699632eb9bc8009d1b84af42bd4b5ba1e1832
evillase <edgar.villasenor@wsu.edu>
2019-07-27 13:21:43 -0800

    Added passport and jwt auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://edgarff:edgarff@cluster0-lfj0g.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://edgarff:edgarff@cluster0-lfj0g.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "secret"
   };
