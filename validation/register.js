c5f377f7e5b10719e4729bb0080cb8a8f4901b66
evillase <edgar.villasenor@wsu.edu>
2019-07-27 12:49:39 -0800

    Added validation for register

---
+++ validation/register.js
@@ -0,0 +1,36 @@
+// pull in dependencies
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+
+// export validate register input function
+module.exports = function validateRegisterInput(data){
+    let errors = {};
+
+    // Convert empty fields to an empty string so we can use validator functions
+    data.name = !isEmpty(data.name) ? data.name: "";
+    data.email = !isEmpty(data.email) ? data.email: "";
+    data.password = !isEmpty(data.password) ? data.password: "";
+    data.password2 = !isEmpty(data.password2) ? data.password2: "";
+
+    // name checks
+    if(Validator.isEmpty(data.name)){
+        errors.name = "Name field is required";
+    }
+
+    // email checks
+    if(Validator.isEmpty(data.email)){
+        errors.email = "Email field is required";
+    } else if(!Validator.isEmail(data.email)){
+        errors.email = "Email is invalid";
+    }
+
+    // password checks
+    if(Validator.isEmpty(data.password)){
+        errors.password = "Password field is required";
+    }
+
+    return{
+        errors,
+        isValid: isEmpty(errors);
+    };
+};
