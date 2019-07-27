dceab63b5893d648028042da71b4289faa03a7cc
evillase <edgar.villasenor@wsu.edu>
2019-07-27 13:28:14 -0800

    Added passport to server file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
+const users = require("./routes/api/users");
 const app = express();
 
 // Bodyparser middleware
@@ -19,6 +21,15 @@ const db = require("./config/keys").mongoURI;
 mongoose.connect(db, {useNewUrlParser: true})
     .then(() => console.log("MongoDB successfully connected"))
     .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port 
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
