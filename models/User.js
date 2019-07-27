c417bb1b053dd2971df296a10262503b87acf6a8
evillase <edgar.villasenor@wsu.edu>
2019-07-27 12:19:38 -0800

    Created User db schema

---
+++ models/User.js
@@ -0,0 +1,26 @@
+// pull in required dependencies
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// export schema
+module.exports = User = mongoose.model("users", UserSchema);
