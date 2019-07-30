a647ef29926383118a4f84cd626265bbf85d5225
evillase <edgar.villasenor@wsu.edu>
2019-07-30 14:21:45 -0800

    Added friend schema

---
+++ models/Friend.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+var ObjectId = mongoose.Schema.Types.ObjectId;
+
+// Schema
+const FriendSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    species: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female"],
+        required: true
+    },
+    breed: {
+        type: String,
+        required: false
+    },
+    neutered: {
+        type: Boolean,
+        required: true
+    },
+    // make reference to UserSchema
+    foster: {
+        type: Schema.Types.ObjectId,
+        ref: "UserSchema"
+    }
+
+});
+
+// export schema
+module.exports = Friend = mongoose.model("friends", FriendSchema);
