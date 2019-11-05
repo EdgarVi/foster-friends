0ba0a01b59a585ff8aec35a2ea664b333a070765
evillase <edgar.villasenor@wsu.edu>
2019-11-05 14:13:30 -0800

    Substantial improvements to Friend's table layout

--- client/src/components/layout/FriendsGallery.js
+++ client/src/components/layout/FriendsGallery.js
@@ -17,48 +17,51 @@ class FriendsGallery extends Component {
             friends: this.props.location.state.data,
             props: this.props
         }, () => console.log(this.state));
+
     }
 
     render() {
         return(
-            <div>
+            <div className="FriendsGallery">
                 <div className="row">
                     <div className="col s3">
                         <Sidenav name={"placeholder name"}/>
                     </div>
-                <div className="col s9" id="Gallery">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col s8 offset-s2">
-                                <div className="col s12">
-                                    <h4 className="center-align">
-                                        <b>Friends</b>
-                                    </h4>
+                    <div className="col s9" id="Gallery">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col s8 offset-s2">
+                                    <div className="col s12">
+                                        <h4 className="center-align">
+                                            <b>Friends</b>
+                                        </h4>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                    <div className="col s12">
+                        <table className="highlight">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Gender</th>
+                                    <th>Species</th>
+                                </tr>
+                            </thead>
+                            <tbody className="Table-body">
+                                {this.state.friends.map((friend) => (
+                                    <tr>
+                                        <th>{friend.name}</th>
+                                        <th>{friend.gender}</th>
+                                        <th>{friend.species}</th>
+                                    </tr> 
+                                ))}
+                            </tbody> 
+                        </table>
                     </div>
-                </div>
-                <div className="col s12">
-                <table className="highlight">
-                    <thead>
-                        <tr>
-                            <th>Gender</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <tbody className="Table-body">
-                    {this.state.friends.map((friend) => (
-                        <tr>
-                            <th>{friend.gender}</th>
-                        </tr> 
-                    ))}
-                </tbody> 
-                    </tbody>
-                    </table>
-                </div>
-                </div>
-            </div>
-        </div>            
+                </div>  
+            </div>      
+        </div>
     )}
 }
 
