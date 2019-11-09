c72557073707fd611ac6664fab4733f6fd7e46df
evillase <edgar.villasenor@wsu.edu>
2019-11-09 14:37:11 -0800

    Added embedded document to schema

--- models/Friend.js
+++ models/Friend.js
@@ -2,6 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
+// Embedded schema
+var FriendInfoSchema = new Schema({
+    about: {
+        type: String, 
+        required: false
+    }
+})
+
 // Schema
 const FriendSchema = new Schema({
     name: {
@@ -26,9 +34,11 @@ const FriendSchema = new Schema({
     owner: {
         type: Schema.Types.ObjectId,
         ref: "user"
-    }
+    }, 
+    friend_info: FriendInfoSchema
 
 });
 
 // export schema
+module.exports = FriendInfoSchema = mongoose.model("friend_info", FriendInfoSchema);
 module.exports = Friend = mongoose.model("friends", FriendSchema);
