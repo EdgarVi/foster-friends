1b70bacdff9d090180c1f610378b19e2492e2af7
evillase <edgar.villasenor@wsu.edu>
2019-12-30 20:07:45 -0600

    Added foster and adoption facts

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -17,8 +17,32 @@ class Dashboard extends Component {
           <Sidenav name={user.name}/>
         </div>
         <div className="col s9">
-          <h2>Welcome {user.name}!</h2>
-          <p>You are logged in</p>
+          <h4 className="center-align">Welcome {this.props.auth.user.name}</h4>
+          <hr/>
+          <div className="">
+            <h5>What does it mean to foster?</h5>
+            <p>By fostering you agree to provide the love, care and attention 
+              all animals deserve. The length of the stay can vary from a few 
+              sleepovers to several weeks depending on the needs of the shelter. 
+            </p>
+            <br/>
+            <h5>How does fostering work?</h5>
+            <p>Animal shelters and foster care organizations enter  
+              friends in to our database. Some programs provide accomodations 
+              such as food, bowls and vouchers for animal care products. We'll make
+              sure to notify you when your foster term is over. If your circumstances change
+              or your ability to care for the animal changes, there is no pressure to 
+              return the animals. 
+            </p>
+            <br/>
+            <h5>How does adopting work?</h5>
+            <p>
+              The adoption process varies between shelters. Typically you will be responsible for 
+              an adoption fee, ensuring that your new friend's vaccines are up to date and 
+              registering for a chip service. Thank you for choosing to adopt instead of shopping!
+            </p>
+            <p></p>
+          </div>
         </div>
       </div>
     </div>
